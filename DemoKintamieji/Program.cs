0be8fd4f4511643ce51d20c06a7232c8612d9145
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-09 11:59:06 +0200

    Add project files.

---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!");
+
+            //komentaras
+
+            /* komentaras 
+             * per kelias
+             * eilutes*/
+            int amzius = 7;
+
+            // Deklaracija ir inicializacija atskirais veiksmais
+            string tekstoKintamasis;
+            tekstoKintamasis = "Labas";
+            Console.WriteLine();
+
+            //paprastiems tipams rekomenduojama nenaudoti var
+        }
+    }
+}
