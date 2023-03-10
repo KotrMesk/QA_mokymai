44f8d6ac282ac90fee83d03e2c610e2f79e59819
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-10 10:59:47 +0200

    ideta demo ivedimas, pakoreguota folderiai

--- DemoIsmetimas/Program.cs
+++ DemoIsmetimas/Program.cs
@@ -10,6 +10,28 @@ namespace DemoIsmetimas
     {
         static void Main(string[] args)
         {
+            var encoding = System.Text.Encoding.UTF8; // kalbos keitimas kad rasytu lietuviskas raides
+            int amzius = 25;
+            Console.WriteLine(amzius);
+
+            //write isveda informacija ir palieka zymekli
+            Console.WriteLine("labas ");
+            Console.WriteLine("rytas");
+
+            //tekstas + kintamasis. 1 budas:
+            Console.WriteLine(13 + " metu");
+
+            //2 budas (panasiai java daro)
+            string vardas = "Kotryna";
+            Console.WriteLine("Mano amzius: {0}", amzius);
+            Console.WriteLine("Mano amzius: {0}, mano vardas: {1}", amzius, vardas);
+
+            //3 budas
+            Console.WriteLine($"Mano amzius: {amzius}");
+            Console.WriteLine($"Mano amzius: {amzius}, mano vardas: {vardas}");
+
+            //vietoj kintamuju galime isvesti operacijos rezultata
+            Console.WriteLine($"Mano amzius: {5+5+20}");
         }
     }
 }
---
+++ DemoIvedimas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoIvedimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite savo varda:");
+            string ivedimas = Console.ReadLine();
+            Console.WriteLine($"Ivedei tai:{ivedimas}");
+
+            //read() bei readline()
+            Console.WriteLine("Iveskite dar ka nors:");
+            Console.WriteLine($"Jus ivedete tai: {Console.ReadLine()}"); //galime ir taip parasyti kad input
+
+            Console.WriteLine("Iveskite simboli: "); //po console.read butina deti console.readline kad neskaitytu nesamoniu ir tolimesnius ivedimus nuskaitytu
+            int symbolAsciiCode = Console.Read(); //nuskaito tik pirma simboli, jei ivedete daugiau simboliu ir antra karta iskviesite console.read() jis tiesiog nuskaitys antra simboli is pirmos eilutes
+            Console.WriteLine($"Ivedete simboli: {((char)symbolAsciiCode)}"); // konvertavimas i char, casting
+            Console.WriteLine($"Ivedi simboli: {Convert.ToChar(symbolAsciiCode)}"); //kreipimasis i console kad konvertuotu
+
+            string skaicius = "5";
+            Convert.ToUInt32(skaicius); //'5'
+            int.Parse(skaicius); //'5'
+        }
+    }
+}
