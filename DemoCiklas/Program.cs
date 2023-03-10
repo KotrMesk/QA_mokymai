8523af59e12540eefc7020c43b5c3e73f7a6dd4f
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-10 12:57:29 +0200

    ciklas loop idetas

---
+++ DemoCiklas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCiklas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (string i = "a"; i != "aaaaaa"; i += "a")
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 5;) // jei nenurodysime operacijos bus nuolatinis loopsas kuris pats nesustos
+            {
+                i++; // jei padesime po console gausime kitoki varianta
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
