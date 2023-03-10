ce9ae4bacc806287247e64654a7c7eed2327cd5c
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-10 09:22:45 +0200

    Demo isvedimas

---
+++ DemoIsvedimas/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amzius = 25;
+            Console.WriteLine(amzius);
+        }
+    }
+}
