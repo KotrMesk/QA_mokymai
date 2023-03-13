27527b3bdfde44b3a2b6161079d25072c08db485
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-13 13:13:09 +0200

    demo list

---
+++ DemoSarasai/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSarasai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //list:
+            //List<tipas> pavadinimas = new List<int>();
+            List<string> zodziai = new List<string>();
+            zodziai.Add("Labas");
+            Console.WriteLine(zodziai[0]);
+        }
+    }
+}
