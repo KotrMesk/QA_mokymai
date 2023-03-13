b0b437c94a7ac65c006bd15021946ca010786aa1
Kotryna Meškauskaitė <meskauskaite.k@gmail.com>
2023-03-13 16:12:05 +0200

    demo klases

---
+++ DemoKlases/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKlases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zmogus zmogusPetras = new Zmogus("Petras", 45, 1.80);
+            Zmogus zmogusSetras = new Zmogus("Saulius");
+
+            Console.WriteLine(zmogusPetras.vardas);
+            Console.WriteLine(zmogusPetras.ugis);
+            Console.WriteLine(zmogusPetras.amzius);
+            Console.WriteLine(zmogusSetras.vardas);
+            Console.WriteLine(zmogusSetras.ugis);
+            Console.WriteLine(zmogusSetras.amzius);
+
+            zmogusPetras.Prisistatymas();
+            zmogusSetras.Atsisveikinimas();
+            zmogusSetras.Atsisveikinimas(zmogusPetras);
+        }
+    }
+}
---
+++ DemoKlases/Zmogus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKlases
+{
+    internal class Zmogus
+    {
+        internal string vardas { get; }
+        internal int amzius { get; }
+        internal double ugis { get; }
+        
+        public Zmogus(string vardas, int amzius, double ugis)
+        {
+            this.vardas = vardas;
+            this.amzius = amzius;
+            this.ugis = ugis;
+        }
+
+        public Zmogus()
+        {
+
+        }
+
+        public Zmogus(string vardas)
+        {
+            this.vardas = vardas;
+        }
+
+        internal void Prisistatymas()
+        {
+   
+            Console.WriteLine($"Sveiki, mano vardas yra {vardas}");
+        }
+
+        internal void Atsisveikinimas()
+        {
+            Console.WriteLine("Viso gero");
+        }
+
+        internal void Atsisveikinimas(Zmogus kitasZmogus)
+        {
+            Console.WriteLine($"Ate {kitasZmogus.vardas}");
+        }
+    }
+}
